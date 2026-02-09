1328b4f2efdf294612d4d6a3d855d123cd9f9644
João Pereira <joaopereira9913@gmail.com>
2026-02-09 10:01:09 +0000

    Changed tables logic: Account -> Strategy -> Trade

--- app/models.py
+++ app/models.py
@@ -1,21 +1,27 @@
 from sqlalchemy.orm import declarative_base, relationship
-from sqlalchemy import Column, Integer, Double, String, ForeignKey
+from sqlalchemy import Column, Integer, Numeric, String, ForeignKey, Boolean
 from app.database import Base
 
-class Trader(Base):
-    __tablename__ = "trader"
+class Account(Base):
+    __tablename__ = "account"
 
     id = Column(Integer, primary_key=True)
+    owner = Column(String, nullable=False)
+    starting_balance = Column(Numeric(12, 2), nullable=False)
+    current_balance = Column(Numeric(12, 2), nullable=False)
+    profit_target = Column(Numeric(12, 2), nullable=False)
+    max_drawdown = Column(Numeric(12, 2), nullable=False)
 
-    strategies = relationship("Strategy", back_populates="trader", cascade="all, delete-orphan")
+    strategies = relationship("Strategy", back_populates="account", cascade="all, delete-orphan")
 
 class Strategy(Base):
     __tablename__ = "strategy"
 
     id = Column(Integer, primary_key=True)
-    trader_id = Column(Integer, ForeignKey("trader.id"),nullable=False)
+    account_id = Column(Integer, ForeignKey("account.id"),nullable=False)
+    name = Column(String(30), nullable=False)
 
-    trader = relationship("Trader", back_populates="strategies")
+    account = relationship("Account", back_populates="strategies")
     trades = relationship("Trade", back_populates="strategy", cascade="all, delete-orphan")
     
 class Trade(Base):
@@ -23,5 +29,7 @@ class Trade(Base):
 
     id = Column(Integer, primary_key=True)
     strategy_id = Column(Integer, ForeignKey("strategy.id"), nullable=False)
+    symbol = Column(String(10), nullable=False)
+    pnl = Column(Numeric(12, 2), nullable=False)
 
     strategy = relationship("Strategy", back_populates="trades")
