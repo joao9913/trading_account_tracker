ab308b76309f8f609cd443ef5d049e6fc2eb0a65
João Pereira <joaopereira9913@gmail.com>
2026-02-13 10:31:30 +0000

    Updated schemas

--- app/schemas.py
+++ app/schemas.py
@@ -5,15 +5,36 @@ class AccountCreate(BaseModel):
     id: int
     owner: str
     starting_balance: float
-    current_balance: float
     profit_target: float
     max_drawdown: float
     starting_date: date
     account_status: str
     strategy_id: int
 
+class AccountRead(BaseModel):
+    id: int
+    owner: str
+    starting_balance: float
+    current_balance: float
+    profit_target: float
+    max_drawdown: float
+    starting_date: date
+    ending_date: date | None
+    account_status: str
+    strategy: StrategyRead
+
+    class Config:
+        from_attributes = True
+
 class StrategyCreate(BaseModel):
     name: str
+
+class StrategyRead(BaseModel):
+    id: int
+    name: str
+
+    class Config:
+        from_attributes = True
 
 class TradeCreate(BaseModel):
     account_id: int
@@ -23,3 +44,14 @@ class TradeCreate(BaseModel):
     close_datetime: datetime
     ticket_id: int
     current_balance: float
+
+class TradeRead(BaseModel):
+    id: int
+    account_id: int
+    symbol: str
+    pnl: float
+    open_datetime: datetime
+    close_datetime: datetime
+
+    class Config:
+        from_attributes = True
